b85628d4a3f12dcb2f25653d467c9762347bf6d6
Ash Wilson <smashwilson@gmail.com>
2013-08-18 15:45:33 -0400

    Alright, let's try to do this with a .compat module.

--- swiftest/account.py
+++ swiftest/account.py
@@ -1,6 +1,7 @@
 import requests
 
 from .exception import AuthenticationError, ProtocolError
+from .compat import to_long
 
 class Account:
     """
@@ -17,8 +18,8 @@ class Account:
 
         if 200 <= meta_response.status_code < 300:
             # Extract metadata from the response.
-            self.container_count = long(meta_response.headers['X-Account-Container-Count'])
-            self.bytes_used = long(meta_response.headers['X-Account-Bytes-Used'])
+            self.container_count = to_long(meta_response.headers['X-Account-Container-Count'])
+            self.bytes_used = to_long(meta_response.headers['X-Account-Bytes-Used'])
         elif 400 <= meta_response.status_code < 500:
             raise AuthenticationError(
                 "Auth token {0} was rejected.".format(client.auth_token))
